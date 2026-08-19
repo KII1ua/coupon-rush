743b3e382d2bb4004f0bff5bc2d4e0106ce9b7b2
KII1ua <sung25480@naver.com>
2026-08-19 11:07:49 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/example/fcfm/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcfm.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
